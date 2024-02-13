e4f806f7255aef02d4c7c46ea6792dd78d2f7a06
Pritish Pai <pritish.pai@databricks.com>
2024-02-13 00:07:21 -0500

    Remove debugging function

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -84,7 +84,3 @@ def test_uploading_notebooks_get_correct_urls(ext, magic, new_installation):
     remote_path = new_installation.upload(f"foo.{ext}", f"{magic}\nprint(1)".encode("utf8"))
     assert f"{new_installation.install_folder()}/foo" == remote_path
 
-
-def test_something(ws, new_installation):
-    ws.workspace_conf.get_status("enableWorkspaceFilesystem")
-    new_installation.save(MyClass("a", "b"))
