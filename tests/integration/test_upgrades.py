9b0b640012f71106fbe84022e9f9bcd90f88ff44
Serge Smertin <serge.smertin@databricks.com>
2024-03-02 13:16:29 +0100

    ..

--- tests/integration/test_upgrades.py
+++ tests/integration/test_upgrades.py
@@ -1,2 +1,13 @@
-def test_upgrades_works():
-    pass
+from pathlib import Path
+
+from databricks.labs.blueprint.installation import Installation
+from databricks.labs.blueprint.wheels import ProductInfo
+
+
+def test_upgrades_works(ws, make_random):
+    simple_upgrades = Path(__file__).parent / "fixtures/simple_upgrades/__init__.py"
+    product_info = ProductInfo(simple_upgrades.as_posix())
+    # Installation.
+    # def test_detect_global(ws, make_random):
+    #     product = make_random(4)
+    #     Installation(ws, product, install_folder=f"/Applications/{product}").upload("some", b"...")
