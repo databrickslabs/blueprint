aa5c0ee77789110ad8e0ea6a54b817e5ba54b341
Serge Smertin <259697+nfx@users.noreply.github.com>
2024-03-09 17:35:46 +0100

    Added commands with interactive prompts (#66)
    If your command needs some terminal interactivity, simply add [`prompts:
    Prompts` argument](#basic-terminal-user-interface-tui-primitives) to
    your command:
    
    ```python
    from databricks.sdk import WorkspaceClient
    from databricks.labs.blueprint.entrypoint import get_logger
    from databricks.labs.blueprint.cli import App
    from databricks.labs.blueprint.tui import Prompts
    
    app = App(__file__)
    logger = get_logger(__file__)
    
    @app.command
    def me(w: WorkspaceClient, prompts: Prompts):
        """Shows current username"""
        if prompts.confirm("Are you sure?"):
            logger.info(f"Hello, {w.current_user.me().user_name}!")
    
    if "__main__" == __name__:
        app()
    ```
    
    Fix #49

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -4,6 +4,17 @@ import sys
 from unittest import mock
 
 from databricks.labs.blueprint.cli import App
+from databricks.labs.blueprint.tui import Prompts
+
+FOO_COMMAND = json.dumps(
+    {
+        "command": "foo",
+        "flags": {
+            "name": "y",
+            "log_level": "disabled",
+        },
+    }
+)
 
 
 def test_commands():
@@ -15,22 +26,23 @@ def test_commands():
         """Some comment"""
         some(name)
 
-    with mock.patch.object(
-        sys,
-        "argv",
-        [
-            ...,
-            json.dumps(
-                {
-                    "command": "foo",
-                    "flags": {
-                        "name": "y",
-                        "log_level": "disabled",
-                    },
-                }
-            ),
-        ],
-    ):
+    with mock.patch.object(sys, "argv", [..., FOO_COMMAND]):
+        app()
+
+    some.assert_called_with("y")
+
+
+def test_injects_prompts():
+    some = mock.Mock()
+    app = App(inspect.getfile(App))
+
+    @app.command(is_unauthenticated=True)
+    def foo(name: str, prompts: Prompts):
+        """Some comment"""
+        assert isinstance(prompts, Prompts)
+        some(name)
+
+    with mock.patch.object(sys, "argv", [..., FOO_COMMAND]):
         app()
 
     some.assert_called_with("y")
