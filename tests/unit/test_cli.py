8300dd7d247090951da38dbaed3acf6e50862e52
vuong-nguyen <44292934+nkvuong@users.noreply.github.com>
2024-05-30 21:20:35 +0100

    fixed `Command.get_argument_type` bug with `UnionType` (#110)
    `Command.get_argument_type` currently crashes when `UnionType` is
    encountered. Add special handling for this type

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -16,6 +16,7 @@ FOO_COMMAND = json.dumps(
             "address": "",
             "is_customer": "true",
             "log_level": "disabled",
+            "optional_arg": "optional",
         },
     }
 )
@@ -26,14 +27,21 @@ def test_commands():
     app = App(inspect.getfile(App))
 
     @app.command(is_unauthenticated=True)
-    def foo(name: str, age: int, salary: float, is_customer: bool, address: str = "default"):
+    def foo(
+        name: str,
+        age: int,
+        salary: float,
+        is_customer: bool,
+        address: str = "default",
+        optional_arg: str | None = None,
+    ):
         """Some comment"""
-        some(name, age, salary, is_customer, address)
+        some(name, age, salary, is_customer, address, optional_arg)
 
     with mock.patch.object(sys, "argv", [..., FOO_COMMAND]):
         app()
 
-    some.assert_called_with("y", 100, 100.5, True, "default")
+    some.assert_called_with("y", 100, 100.5, True, "default", "optional")
 
 
 def test_injects_prompts():
@@ -41,12 +49,20 @@ def test_injects_prompts():
     app = App(inspect.getfile(App))
 
     @app.command(is_unauthenticated=True)
-    def foo(name: str, age: int, salary: float, is_customer: bool, prompts: Prompts, address: str = "default"):
+    def foo(
+        name: str,
+        age: int,
+        salary: float,
+        is_customer: bool,
+        prompts: Prompts,
+        address: str = "default",
+        optional_arg: str | None = None,
+    ):
         """Some comment"""
         assert isinstance(prompts, Prompts)
-        some(name, age, salary, is_customer, address)
+        some(name, age, salary, is_customer, address, optional_arg)
 
     with mock.patch.object(sys, "argv", [..., FOO_COMMAND]):
         app()
 
-    some.assert_called_with("y", 100, 100.5, True, "default")
+    some.assert_called_with("y", 100, 100.5, True, "default", "optional")
