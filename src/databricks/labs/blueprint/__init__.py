446cba554c31157aff768e7ef03b33f54bb4d794
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2025-04-16 16:24:37 +0200

    Fixed Blueprint Install (#225)
    Fixes Blueprint Install

--- src/databricks/labs/blueprint/__init__.py
+++ src/databricks/labs/blueprint/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from databricks.sdk.core import with_user_agent_extra
 
-from .__about__ import __version__
+from databricks.labs.blueprint.__about__ import __version__
 
 with_user_agent_extra("blueprint", __version__)
 
