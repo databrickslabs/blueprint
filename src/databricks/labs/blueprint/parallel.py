8bd385592bb71f62d1b2dd3d5813eb059494b3c0
Andrew Snare <andrew.snare@databricks.com>
2025-06-03 15:44:51 +0200

    On older versions of Python, sometimes (esp. Linux) the number of available CPUs is available elsewhere.

--- src/databricks/labs/blueprint/parallel.py
+++ src/databricks/labs/blueprint/parallel.py
@@ -44,6 +44,9 @@ class Threads(Generic[Result]):
         # From Python 3.13, this is solved for us by the os module.
         if hasattr(os, "process_cpu_count"):
             return getattr(os, "process_cpu_count")()
+        # For earlier versions, on Linux we can use sched_getaffinity to get the set of available CPUs.
+        if hasattr(os, "sched_getaffinity"):
+            return len(os.sched_getaffinity(0))
         # Otherwise use the number of CPUs in the system, if known, even if not all are available.
         return os.cpu_count() or 1
 
