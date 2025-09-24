f9329c95d062720d3459400202a4f37c2379a73e
sundarshankar89 <sundar.shankar@databricks.com>
2025-09-24 20:37:16 +0530

    addressing review comments

--- tests/unit/test_installation.py
+++ tests/unit/test_installation.py
@@ -764,21 +764,21 @@ def test_forward_referencing_class() -> None:
 
 
 def test_bool_attribute() -> None:
-    @dataclass
+    @dataclass(kw_only=True)
     class BooleanAttributeClass:
         __file__ = "config.yml"
         __version__ = 3
-        skip_validation: bool = True
-        sdk_config: JsonValue = None
+        skip_validation: bool
+        sdk_config: JsonValue
 
-    instance_false = BooleanAttributeClass(False, {"warehouse_id": "8xc123456"})
+    instance_false = BooleanAttributeClass(skip_validation=False, sdk_config={"warehouse_id": "8xc123456"})
     installation = MockInstallation()
     installation.save(instance_false)
 
     loaded_false = installation.load(BooleanAttributeClass)
     assert instance_false == loaded_false
 
-    instance_true = BooleanAttributeClass(True, {"warehouse_id": "9xc1234567"})
+    instance_true = BooleanAttributeClass(skip_validation=True, sdk_config={"warehouse_id": "9xc1234567"})
     installation.save(instance_true)
 
     loaded_true = installation.load(BooleanAttributeClass)
